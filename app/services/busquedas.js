ab98607b3c3f9ec0cf8076bd03d803d01f402982
medicavial <salcala@medicavial.com.mx>
2017-06-20 14:00:27 -0500

    listado autorizaciones
    nada

--- app/services/busquedas.js
+++ app/services/busquedas.js
@@ -2,6 +2,9 @@ app.factory("busquedas", function($http, $rootScope){
     return{
         buscaAutorizaciones:function(datos){
             return $http.get('http://172.17.10.58/apiseg/public/api/busqueda/busquedaAutorizaciones');
+        },
+         buscaUnidades:function(){
+            return $http.get('http://172.17.10.58/apiseg/public/api/busqueda/busquedaUnidades');
         }
     }
 });
